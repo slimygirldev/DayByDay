7e3d1aac5c47cc35e2b3985df989e21d2a9536b4
Lorene Brocourt <lorenebrocourt@gmail.com>
2022-11-28 16:54:17 +0100

    edit method for auth and database managers

--- DayByDay/Managers/AuthManager.swift
+++ DayByDay/Managers/AuthManager.swift
@@ -15,5 +15,54 @@ final class AuthManager {
 
     private init() {}
 
+    public var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+
+    public func signUp(email: String,
+                       password: String,
+                       completion: @escaping (Bool) -> Void) {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6 else {
+            return
+        }
+        auth.createUser(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                completion(false)
+                return
+            }
+            // account created
+            completion(true)
+        }
+    }
+
+    public func signIn(email: String,
+                       password: String,
+                       completion: @escaping (Bool) -> Void) {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6 else {
+            return
+        }
+        auth.signIn(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        }
+    }
+
+    public func signOut(completion: (Bool) -> Void) {
+        do {
+            try auth.signOut()
+            completion(true)
+        }
+        catch {
+            print(error)
+            completion(false)
+        }
+    }
 
 }
